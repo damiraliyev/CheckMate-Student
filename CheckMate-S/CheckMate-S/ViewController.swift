1a98f685583c0415cfd00c1c48b2fc8d60effbb0
damiraliyev <damir.aliyev.02@mail.ru>
2023-03-03 16:44:16 +0600

    add project and remove storyboard

---
+++ CheckMate-S/CheckMate-S/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CheckMate-S
+//
+//  Created by Damir Aliyev on 03.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .orange
+    }
+
+
+}
+
